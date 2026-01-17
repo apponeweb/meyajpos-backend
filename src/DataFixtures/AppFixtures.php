4e32a8c3126073529ce1cd6854ab43345b7df2e4
Dairo <dairo@dgtalliance.com>
2026-01-17 15:26:11 -0500

    Generando datos

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,33 +2,104 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\PaymentType;
+use App\Entity\ServiceType;
 use App\Entity\User;
+use App\Entity\Company;
+use App\Entity\Branch;
+use App\Entity\CashBox;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-
-
     public function __construct(private readonly UserPasswordHasherInterface $userPasswordHasher)
     {
     }
 
     public function load(ObjectManager $manager): void
     {
+        // 1. Usuario
         $user = new User();
         $user->setName('Administrador');
         $user->setEmail('admin@admin.com');
         $user->setRoles(['ROLE_ADMIN']);
-        // hash the password (based on the security.yaml config for the $user class)
-        $hashedPassword = $this->userPasswordHasher->hashPassword(
-            $user,
-            'admin'
-        );
-
-        $user->setPassword($hashedPassword);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'admin'));
         $manager->persist($user);
+
+        // 2. Empresa
+        $company = new Company();
+        $company->setName('Corporativo Alpha');
+        $company->setAcronym('ALPHA');
+        $company->setLegalName('Alpha Systems S.A.');
+        $company->setRfc('ALP123456XYZ');
+        $manager->persist($company);
+
+        $company1 = new Company();
+        $company1->setName('Enterprise S.A.');
+        $company1->setAcronym('ENTE');
+        $company1->setLegalName('Enterprise S.A.');
+        $company1->setRfc('ENT569456XYZ');
+        $manager->persist($company1);
+
+        // 3. Sucursales
+        $branches = [];
+        $branchNames = ['Sucursal Centro', 'Sucursal Poniente'];
+
+        foreach ($branchNames as $index => $name) {
+            $branch = new Branch();
+            $branch->setName($name);
+            $branch->setAcronym('BR' . ($index + 1));
+            $branch->setAddress('Dirección conocida #' . ($index + 1));
+            $branch->setCompany($company);
+            $manager->persist($branch);
+            $branches[] = $branch;
+        }
+
+        // 4. Cajas (CashBox) - Generamos 2 cajas por cada sucursal
+        foreach ($branches as $branch) {
+            for ($i = 1; $i <= 2; $i++) {
+                $cashBox = new CashBox();
+                // El campo 'name' viene del NomenclatorTrait
+                $cashBox->setName("Caja " . $i . " - " . $branch->getName());
+                $cashBox->setBranch($branch);
+                $cashBox->setTicketSerie("SERIE-" . strtoupper(substr($branch->getName(), 0, 3)));
+                $cashBox->setCurrentFolio(0); // Iniciamos el folio en cero
+
+                $manager->persist($cashBox);
+            }
+        }
+
+        $serviceType1 = new ServiceType();
+        $serviceType1->setName('CORTE');
+        $serviceType1->setDescription('CORTE');
+        $manager->persist($serviceType1);
+
+        $serviceType2 = new ServiceType();
+        $serviceType2->setName('BARBA');
+        $serviceType2->setDescription('BARBA');
+        $manager->persist($serviceType2);
+
+        $serviceType3 = new ServiceType();
+        $serviceType3->setName('CEJA');
+        $serviceType3->setDescription('CEJA');
+        $manager->persist($serviceType2);
+
+
+        $paymentType1 = new PaymentType();
+        $paymentType1->setName('Débito');
+        $paymentType1->setDescription('Débito');
+        $paymentType1->setReferenceRequired(true);
+        $manager->persist($paymentType1);
+
+        $paymentType2 = new PaymentType();
+        $paymentType2->setName('Efectivo');
+        $paymentType2->setDescription('Efectivo');
+        $paymentType2->setReferenceRequired(false);
+        $manager->persist($paymentType2);
+
+
         $manager->flush();
     }
 }
