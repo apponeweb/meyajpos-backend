9072f814c7880158b7010a4cdfc5f269df500e31
Dairo <dairo@dgtalliance.com>
2026-01-22 05:28:25 -0500

    Ajustes

--- src/Form/Type/CommissionDetailFormType.php
+++ src/Form/Type/CommissionDetailFormType.php
@@ -12,7 +12,6 @@ use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\EnumType;
 
 class CommissionDetailFormType extends AbstractType
 {
