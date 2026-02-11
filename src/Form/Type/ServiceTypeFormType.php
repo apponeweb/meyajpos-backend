4d4d693d1f8cb0b95a36b7e34b25b957954d28dc
Dairo <dairo@dgtalliance.com>
2026-02-11 15:44:25 -0500

    Ajustes

--- src/Form/Type/ServiceTypeFormType.php
+++ src/Form/Type/ServiceTypeFormType.php
@@ -7,11 +7,21 @@ use App\Entity\Company;
 use App\Entity\ServiceType;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ServiceTypeFormType extends BaseFormType
 {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        // 1. Ejecuta la lógica del padre (agrega automáticamente name y description)
+        parent::buildForm($builder, $options);
+        $builder->add('isCourtesy', CheckboxType::class, ['required' => false]);
+    }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
